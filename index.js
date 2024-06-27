5d8341d53fc845912970bb6e63202c3c4fca52c5
Weddy Nkatha <weddynkatha265@gmail.com>
2024-06-27 13:23:34 +0300

    Updates functions lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  // Scuber's headquarters is on 42nd Street
+  const hqLocation = 42;
+
+  // Calculate the absolute difference between pickupLocation and hqLocation
+  const blocksAway = Math.abs(pickupLocation - hqLocation);
+
+  return blocksAway;
+}
+
+// Example usage:
+console.log(distanceFromHqInBlocks(50)); // Should return 8
+console.log(distanceFromHqInBlocks(43)); // Should return 1
+console.log(distanceFromHqInBlocks(34)); // Should return 8
+
+function distanceFromHqInFeet(blocksAway) {
+  const distance = distanceFromHqInBlocks(blocksAway) * 264;
+  return distance;
+}
+console.log(distanceFromHqInFeet(43));
+console.log(distanceFromHqInFeet(50));
+console.log(distanceFromHqInFeet(34));
+
+function distanceTravelledInFeet(start, destination) {
+  const feet = Math.abs((destination - start) * 264);
+  return feet;
+}
+console.log(distanceTravelledInFeet(43, 48));
+console.log(distanceTravelledInFeet(43, 48));
+console.log(distanceTravelledInFeet(43, 48));
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+console.log(calculatesFarePrice(43, 44));
+console.log(calculatesFarePrice(34, 32));
+console.log(calculatesFarePrice(50, 58));
+console.log(calculatesFarePrice(34, 24));
